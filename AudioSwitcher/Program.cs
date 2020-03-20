572472cfa6c3453360df52d4c65cd2a572804d06
Jared DuPont <jrddupont@gmail.com>
2020-03-20 14:29:29 -0500

    Updated code to work automatically

--- AudioSwitcher/Program.cs
+++ AudioSwitcher/Program.cs
@@ -4,199 +4,300 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
+using System.IO.Ports;
 using System.Linq;
-using System.Text;
+using System.Security.Cryptography;
+using System.Threading;
 using System.Windows.Forms;
 
-namespace AudioSwitcher
-{
-    public class SysTrayApp : Form
-    {
-        [STAThread]
-        public static void Main()
-        {
-            Application.Run(new SysTrayApp());
-        }
+namespace AudioSwitcher {
+	public class SysTrayApp: Form {
+		const string controllerExePath = "EndPointController.exe";
 
-        private NotifyIcon trayIcon;
-        private ContextMenu trayMenu;
-        private int deviceCount;
-        private int currentDeviceId;
-        private static List<int> qDevices;
+		[STAThread]
+		public static void Main(params string[] args) {
+			if ( args.Length > 0 && args[0] == "--update-md5" ) {
+				if ( System.IO.File.Exists(controllerExePath) ) {
+					CreateControllerExeHash(controllerExePath);
+					MessageBox.Show(Resources.MESSAGE_MD5_UPDATED, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+				} else {
+					MessageBox.Show(Resources.ERROR_CONTROLLER_NOT_FOUND, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			} else {
+				string errorMessage = "";
+				if ( !System.IO.File.Exists(controllerExePath) ) {
+					errorMessage = Resources.ERROR_CONTROLLER_NOT_FOUND + "\n" + Resources.MESSAGE_APPLICATION_WILL_BE_CLOSED;
+				} else if ( Settings.Default.EndPointControllerMD5CheckSumExpect == "" ) {
+					errorMessage = Resources.ERROR_NO_MD5;
+				} else if ( !HasCorrectHash(controllerExePath) ) {
+					errorMessage = Resources.ERROR_INCORRECT_HASH + "\n" + Resources.MESSAGE_APPLICATION_WILL_BE_CLOSED;
+				}
 
+				if ( errorMessage != "" ) {
+					MessageBox.Show(errorMessage, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				} else {
+					// If everything worked, this is the starting point
+					Application.Run(new SysTrayApp());
+					
+				}
+			}
+		}
 
-        public SysTrayApp()
-        {
-            qDevices = Settings.Default.ChoosedDevices ?? new List<int>();
-            // Create a simple tray menu
-            trayMenu = new ContextMenu();
+		private readonly NotifyIcon trayIcon;
+		private readonly ContextMenu trayMenu;
 
-            // Create a tray icon
-            trayIcon = new NotifyIcon();
-            trayIcon.Text = "AudioSwitcher";
-            trayIcon.Icon = new Icon(Resources.speaker, 40, 40);
+		private Thread SerialThread;
 
-            // Add menu to tray icon and show it
-            trayIcon.ContextMenu = trayMenu;
-            trayIcon.Visible = true;
+		public SysTrayApp() {
 
-            // Count sound-devices
-            foreach (var tuple in GetDevices())
-            {
-                deviceCount += 1;
-            }
+			SerialThread = new Thread(InitializeSerialComms);
+			SerialThread.Start();
 
-            // Populate device list when menu is opened
-            trayIcon.ContextMenu.Popup += PopulateDeviceList;
+			// Create a simple tray menu
+			trayMenu = new ContextMenu();
 
-            // Register MEH on trayicon leftclick
-            trayIcon.MouseUp += new MouseEventHandler(TrayIcon_LeftClick);
-        }
+			// Create a tray icon
+			trayIcon = new NotifyIcon();
+			trayIcon.Text = "AudioSwitcher";
+			trayIcon.Icon = new Icon(Resources.speaker, 40, 40);
 
-        // Selects next device in list when trayicon is left-clicked
-        private void TrayIcon_LeftClick(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                var cur_id = nextId();
-                SelectDevice(cur_id);
-                foreach (var tuple in GetDevices().Where(tuple => cur_id == tuple.Item1))
-                {
-                    trayIcon.Text = "Playing: " + tuple.Item2;
-                    break;
-                }
-            }
-        }
-
-
-        public static T NextOf<T>(IList<T> list, T item)
-        {
-            return list[(list.IndexOf(item) + 1) == list.Count ? 0 : (list.IndexOf(item) + 1)];
-        }
-
-        //Gets the ID of the next sound device in the list
-        private int nextId()
-        {
-            if (qDevices.Count>0) currentDeviceId = NextOf(qDevices, currentDeviceId);
-            return currentDeviceId;
-        }
+			// Make it so we can right click the icon, and show it
+			trayIcon.ContextMenu = trayMenu;
+			var exitItem = new MenuItem { Text = Resources.LABEL_EXIT };
+			exitItem.Click += OnExit;
+			trayMenu.MenuItems.Add(exitItem);
+			trayIcon.Visible = true;
+		}
 
 
+		#region Serial COM 
 
-        #region Tray events
+		static SerialPort chosenPort = null;
+		private static bool continueRunning = true;
 
-        private void PopulateDeviceList(object sender, EventArgs e)
-        {
-            // Empty menu to prevent stuff to pile up
-            trayMenu.MenuItems.Clear();
+		private static void InitializeSerialComms() {
 
-            // All all active devices
-            foreach (var tuple in GetDevices())
-            {
-                var id = tuple.Item1;
-                var deviceName = tuple.Item2;
-                var isInUse = qDevices.Contains(id);
+			while ( continueRunning ) {
+				// Get a list of serial port names.
+				string[] ports = SerialPort.GetPortNames().Distinct().ToArray();
 
-                var item = new MenuItem {Checked = isInUse, Text = deviceName + " ("+id+")"};
-                item.Click += (s, a) => AddDeviceToList(id);
+				foreach ( string port in ports ) {
+					Console.WriteLine("Scanning port: " + port);
 
-                trayMenu.MenuItems.Add(item);
-            }
+					// Create a new SerialPort object with default settings.
+					chosenPort = new SerialPort {
+						PortName = port,
+						BaudRate = 9600,
+						ReadTimeout = 1500,
+						WriteTimeout = 1500,
+						NewLine = "\r\n",
+						DtrEnable = true
+					};
 
-            // Add an exit button
-            var exitItem = new MenuItem {Text = "Exit"};
-            exitItem.Click += OnExit;
-            trayMenu.MenuItems.Add("-");
-            trayMenu.MenuItems.Add(exitItem);
-        }
+					try {
+						chosenPort.Open();
+					} catch {
+						Console.WriteLine("Failed to open port");
+						chosenPort = null;
+						continue;
+					}
 
-        private static void AddDeviceToList(int id)
-        {
-            if (qDevices.Contains(id)) qDevices.Remove(id);
-            else qDevices.Add(id);
-        }
+					// Start the handshake 
+					chosenPort.Write("Are you a headphone holder?\n");
 
-        #endregion
+					string response;
+					try {
+						response = chosenPort.ReadLine().Trim();
+					} catch ( TimeoutException ) {
+						Console.WriteLine("Port did not respond to handshake request");
+						chosenPort = null;
+						continue;
+					}
 
-        #region EndPointController.exe interaction
+					if ( response.Equals("Yes I am!") ) {
+						Console.WriteLine("Successfully established handshake connection.");
+						break;
+					} else {
+						Console.WriteLine("Unrecognized handshake response: " + response);
+						chosenPort.Close();
+						chosenPort = null;
+						continue;
+					}
+				}
 
-        private static IEnumerable<Tuple<int, string, bool>> GetDevices()
-        {
-            var p = new Process
-                        {
-                            StartInfo =
-                                {
-                                    UseShellExecute = false,
-                                    RedirectStandardOutput = true,
-                                    CreateNoWindow = true,
-                                    FileName = "EndPointController.exe",
-                                    Arguments = "-f \"%d|%ws|%d|%d\""
-                                }
-                        };
-            p.Start();
-            p.WaitForExit();
-            var stdout = p.StandardOutput.ReadToEnd().Trim();
+				if ( chosenPort == null ) {
+					Console.WriteLine("No headphone holders found!");
+					Thread.Sleep(1000);
+					InitializeSerialComms();
+				} else {
+					Read();
+				}
+			}
 
-            var devices = new List<Tuple<int, string, bool>>();
 
-            foreach (var line in stdout.Split('\n'))
-            {
-                var elems = line.Trim().Split('|');
-                var deviceInfo = new Tuple<int, string, bool>(int.Parse(elems[0]), elems[1], elems[3].Equals("1"));
-                devices.Add(deviceInfo);
-            }
+		}
 
-            return devices;
-        }
+		public static void Read() {
+			while ( continueRunning ) {
+				try {
+					chosenPort.Write("Docked?\n");
+					string message = chosenPort.ReadLine();
+					Console.WriteLine(message);
+					switch ( message ) {
+						case "Yes!": HeadphoneEvent(true); break;
+						case "No!": HeadphoneEvent(false); break;
+					}
+				} catch ( TimeoutException ) {
+					// These are normal, ignore them 
+				} catch {
+					try {
+						chosenPort.Close();
+					} catch { }
+					InitializeSerialComms();
+					break;
+				}
+				Thread.Sleep(250);
+			}
+		}
 
-        private static void SelectDevice(int id)
-        {
+		#endregion
 
-            var p = new Process
-            {
-                StartInfo =
-                                {
-                                    UseShellExecute = false,
-                                    RedirectStandardOutput = true,
-                                    CreateNoWindow = true,
-                                    FileName = "EndPointController.exe",
-                                    StandardOutputEncoding = Encoding.UTF8,
-                                    Arguments = id.ToString(CultureInfo.InvariantCulture)
-                                }
-            };
-            p.Start();
-            p.WaitForExit();
-        }
-        #endregion
+		public static bool lastDockedState = false;
+		private static void HeadphoneEvent(bool isDocked) {
+			if (lastDockedState != isDocked) {
+				lastDockedState = isDocked;
 
-        #region Main app methods
+				var devices = GetDevices();
+				var deviceNameSearchString = "";
+				if ( isDocked ) {
+					deviceNameSearchString = "[S]";
+					Console.WriteLine("Switching to speakers...");
+				} else {
+					deviceNameSearchString = "[H]";
+					Console.WriteLine("Switching to headphones...");
+				}
+				var deviceToSwitchTo = devices.FirstOrDefault(device => device.DeviceName.StartsWith(deviceNameSearchString));
+				ChangeSoundDeviceTo(deviceToSwitchTo.DeviceID);
+			}
+		}
 
-        protected override void OnLoad(EventArgs e)
-        {
-            Visible = false; // Hide form window.
-            ShowInTaskbar = false; // Remove from taskbar.
 
-            base.OnLoad(e);
-        }
 
-        private void OnExit(object sender, EventArgs e)
-        {
-            Settings.Default.ChoosedDevices = qDevices;
-            Settings.Default.Save();
-            Application.Exit();
-        }
+		#region EndPointController.exe interaction
 
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                // Release the icon resource.
-                trayIcon.Dispose();
-            }
+		struct AudioDevice {
+			public int DeviceID;
+			public string DeviceName;
+			public bool IsActive;
+			public AudioDevice(int deviceID, string deviceName, bool isActive) {
+				DeviceID = deviceID;
+				DeviceName = deviceName;
+				IsActive = isActive;
+			}
+		}
 
-            base.Dispose(isDisposing);
-        }
+		// Returns a list of devices in (ID, Name, IsActive) format
+		private static IEnumerable<AudioDevice> GetDevices() {
+			var devices = new List<AudioDevice>();
 
-        #endregion
-    }
+			if ( !System.IO.File.Exists(controllerExePath) || !HasCorrectHash(controllerExePath) ) {
+				MessageBox.Show(Resources.ERROR_CONTROLLER_CHANGED + "\n" + Resources.MESSAGE_APPLICATION_WILL_BE_CLOSED, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Application.Exit();
+			} else {
+				var p = new Process {
+					StartInfo = {
+						UseShellExecute = false,
+						RedirectStandardOutput = true,
+						CreateNoWindow = true,
+						FileName = controllerExePath,
+						Arguments = "-f \"%d|%ws|%d|%d\""
+					}
+				};
+				p.Start();
+				p.WaitForExit();
+				var stdout = p.StandardOutput.ReadToEnd().Trim();
+
+				foreach ( var line in stdout.Split('\n') ) {
+					var elems = line.Trim().Split('|');
+					var deviceInfo = new AudioDevice(int.Parse(elems[0]), elems[1], elems[3].Equals("1"));
+					devices.Add(deviceInfo);
+				}
+			}
+
+			return devices;
+		}
+
+		private static void ChangeSoundDeviceTo(int id) {
+			if ( System.IO.File.Exists(controllerExePath) && HasCorrectHash(controllerExePath) ) {
+				var p = new Process {
+					StartInfo = {
+						UseShellExecute = false,
+						RedirectStandardOutput = true,
+						CreateNoWindow = true,
+						FileName = controllerExePath,
+						Arguments = id.ToString(CultureInfo.InvariantCulture)
+					}
+				};
+				p.Start();
+				p.WaitForExit();
+			} else {
+				MessageBox.Show(Resources.ERROR_CONTROLLER_CHANGED + "\n" + Resources.MESSAGE_APPLICATION_WILL_BE_CLOSED, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Application.Exit();
+			}
+		}
+
+		#endregion
+
+		#region Main app methods
+
+		protected override void OnLoad(EventArgs e) {
+			Visible = false; // Hide form window.
+			ShowInTaskbar = false; // Remove from taskbar.
+
+			base.OnLoad(e);
+		}
+
+		private void OnExit(object sender, EventArgs e) {
+			continueRunning = false;
+			if (chosenPort != null ) {
+				chosenPort.Close();
+			}
+			Application.Exit();
+		}
+
+		protected override void Dispose(bool isDisposing) {
+			if ( isDisposing ) {
+				// Release the icon resource.
+				trayIcon.Dispose();
+			}
+
+			base.Dispose(isDisposing);
+		}
+
+		#endregion
+
+		#region Program security: EndPointController.exe validation
+
+		private static void CreateControllerExeHash(string controllerExePath) {
+			Settings.Default.EndPointControllerMD5CheckSumExpect = ComputeMD5Checksum(controllerExePath);
+			Settings.Default.Save();
+		}
+
+		private static bool HasCorrectHash(string controllerExePath) {
+			return Settings.Default.EndPointControllerMD5CheckSumExpect == ComputeMD5Checksum(controllerExePath);
+		}
+
+		private static string ComputeMD5Checksum(string filePath) {
+			using ( System.IO.FileStream fs = System.IO.File.OpenRead(filePath) ) {
+				MD5 md5 = new MD5CryptoServiceProvider();
+				byte[] fileData = new byte[fs.Length];
+				fs.Read(fileData, 0, (int)fs.Length);
+				byte[] checkSum = md5.ComputeHash(fileData);
+				string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+				return result;
+			}
+		}
+
+		#endregion
+	}
 }
